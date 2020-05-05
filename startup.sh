0e3873b6be096240543f04660f96299a7eaf23d6
MOBIUS <hd2dj07@gmail.com>
2020-05-05 11:25:36 +0900

    modify shell script

--- startup.sh
+++ startup.sh
@@ -10,9 +10,7 @@ cd /backend
 
 #python3 -m pip install -r requirement.txt
 #python3 manage.py collectstatic --noinput -i yes
-#python3 manage.py makemigrations accounts
 #python3 manage.py makemigrations radio
-#python3 manage.py migrate accounts --database=user
 #python3 manage.py migrate
 
 
@@ -24,3 +22,4 @@ cd /backend
 #done
 #uwsgi --show-config
 exec "$@"
+
