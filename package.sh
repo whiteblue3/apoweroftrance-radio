5a1e960e7280148c9783fa7a8f5cb17dc8d7cc84
MOBIUS <hd2dj07@gmail.com>
2020-05-12 21:12:37 +0900

    세팅 수정

--- package.sh
+++ package.sh
@@ -6,4 +6,4 @@ VERSION=0.0.1
 cd ${SCRIPTDIR}/radio/
 python3 setup.py sdist bdist_wheel
 cp ${SCRIPTDIR}/radio/dist/apoweroftrance-radio-${VERSION}.tar.gz ${SCRIPTDIR}/upload/
-#python3 -m pip install ${SCRIPTDIR}/upload/apoweroftrance-radio-${VERSION}.tar.gz
+python3 -m pip install ${SCRIPTDIR}/upload/apoweroftrance-radio-${VERSION}.tar.gz
